9f30bcf127c7ed6505d28ededa2d8142e6c25607
KaveeshaKularathne <kaveesha@virtuan.io>
2020-11-24 12:44:09 +0530

    Cart Update

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,17 @@
 export const products = [
     {
-      name: 'Phone XL',
+      name: 'IPhone Max',
       price: 799,
       description: 'A large phone with one of the best screens'
     },
     {
-      name: 'Phone Mini',
+      name: 'Phone  12 Mini',
       price: 699,
       description: 'A great phone with one of the best cameras'
     },
     {
-      name: 'Phone Standard',
+      name: 'Phone 11',
       price: 299,
-      description: ''
+      description: 'Good qality phone with different colours'
     }
   ];
